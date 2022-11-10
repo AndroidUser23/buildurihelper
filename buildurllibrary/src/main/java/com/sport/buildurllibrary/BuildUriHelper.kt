2164e2274b4285e2d3af0b052faf965c20124fc2
Nazar Taraniuk <n.taraniuk@pin-up.team>
2022-11-10 13:13:03 +0200

    updated library 1.4.0

--- buildurllibrary/src/main/java/com/sport/buildurllibrary/BuildUriHelper.kt
+++ buildurllibrary/src/main/java/com/sport/buildurllibrary/BuildUriHelper.kt
@@ -10,7 +10,7 @@ class BuildUriHelper {
     fun buildFinalUri(baseUrl: String,deep: String?, apps: MutableMap<String, Any>?, application: Application): String =
         baseUrl.toUri().buildUpon().apply {
             appendQueryParameter("sgjejk", AppsFlyerLib.getInstance().getAppsFlyerUID(application)
-                    + "_" + application.packageName + "_" + "NfigqA5UE8eYWyTMeuStvN")
+                    + "_" + application.packageName + "_" + "Vc7oAwH5RoHjoUa63h6DL5")
             appendQueryParameter("rykdh", apps?.get("campaign").toString())
             appendQueryParameter("kghsgj", deep)
         }.toString()
